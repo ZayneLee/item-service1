b1f3dc23c4a9342bd3b1d016168fba496b4d6a1f
Zayne <star1582@naver.com>
2023-02-02 22:34:54 +0900

    상품등록 Controller 작업

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -48,7 +49,15 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String save(@RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        return "xxx";
     }
+
 }
